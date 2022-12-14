0a32d06a45a2fc07583a82863827046c15dde3c3
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2022-12-14 15:48:32 +1000

    updated post-deployment-script

--- scripts/PurviewPostDeploy.ps1
+++ scripts/PurviewPostDeploy.ps1
@@ -91,6 +91,71 @@ while (-not $completed) {
 }
 
 #------------------------------------------------------------------------------------------------------------
+# ADD UAMIIdentityID TO DATA SOURCE ADMINISTRATOR ROLE
+#------------------------------------------------------------------------------------------------------------
+
+#Call Data Plane API to add UAMI as Data Source Administrator
+$token = (Get-AzAccessToken -Resource "https://purview.azure.net").Token
+$headers = @{ Authorization = "Bearer $token" }
+
+$PolicyId = ""
+$uri = "https://$PurviewAccountName.purview.azure.com/policystore/metadataPolicies/`?api-version=2021-07-01-preview"
+
+$retrycount = 1
+$completed = $false
+
+while (-not $completed) {
+  try {
+    #Retrieve Purview default metadata policy ID
+    Write-Host "List Metadata Policies..."
+    $result = Invoke-RestMethod -Method Get -ContentType "application/json" -Uri $uri -Headers $headers -Body $body -ErrorAction Stop
+    $PolicyId = $result.values.Id
+
+    Write-Host "Retrieve metadata policy (ID $PolicyId) details..."
+    $uri = "https://$PurviewAccountName.purview.azure.com/policystore/metadataPolicies/$PolicyId`?api-version=2021-07-01-preview"
+
+    #Retrieve Metadata Policy details 
+    $result = Invoke-RestMethod -Method Get -ContentType "application/json" -Uri $uri -Headers $headers -Body $body -ErrorAction Stop
+
+
+    foreach ($attributeRule in $result.properties.attributeRules) {
+      #Add Deployment Script UAMI PrincipalID to Data Source Administrator Role.
+      if ($attributeRule.id -like "*data-source-administrator*") {
+        if (-not ($attributeRule.dnfCondition[0][0].attributeValueIncludedIn -contains $UAMIIdentityID)) {
+          $attributeRule.dnfCondition[0][0].attributeValueIncludedIn += $UAMIIdentityID  
+        }
+      
+      } elseif ($attributeRule.id -like "*data-curator*") {
+        #Add Data Share PrincipalID to Data Curator Role.
+        if (-not ([string]::IsNullOrEmpty($DataShareIdentityID)) -and -not ($attributeRule.dnfCondition[0][0].attributeValueIncludedIn -contains $DataShareIdentityID)) {
+          $attributeRule.dnfCondition[0][0].attributeValueIncludedIn += $DataShareIdentityID  
+        }
+        if (-not ([string]::IsNullOrEmpty($SynapseWorkspaceIdentityID)) -and -not ($attributeRule.dnfCondition[0][0].attributeValueIncludedIn -contains $SynapseWorkspaceIdentityID)) {
+          $attributeRule.dnfCondition[0][0].attributeValueIncludedIn += $SynapseWorkspaceIdentityID
+        }
+      }
+    }
+
+    #Update Metadata Policy
+    Write-Host "Update metadata policy (ID $PolicyId)..."
+    $body = ConvertTo-Json -InputObject $result -Depth 10
+    Invoke-RestMethod -Method Put -ContentType "application/json" -Uri $uri -Headers $headers -Body $body -ErrorAction Stop
+    $completed = $true
+  }
+  catch {
+    if ($retrycount -ge $retries) {
+        Write-Host "Metadata policy update failed the maximum number of $retryCount times."
+        throw
+    } else {
+        Write-Host "Metadata policy update failed $retryCount time(s). Retrying in $secondsDelay seconds."
+        Write-Warning $Error[0]
+        Start-Sleep $secondsDelay
+        $retrycount++
+    }
+  }
+}
+
+#------------------------------------------------------------------------------------------------------------
 # CREATE KEY VAULT CONNECTION
 #------------------------------------------------------------------------------------------------------------
 
@@ -115,7 +180,7 @@ $completed = $false
 
 while (-not $completed) {
   try {
-    Invoke-RestMethod -Method Put -ContentType "application/json" -Uri $uri -Headers $headers -Body $body -ErrorAction Stop
+    $result = Invoke-RestMethod -Method Put -ContentType "application/json" -Uri $uri -Headers $headers -Body $body -ErrorAction Stop
     Write-Host "KeyVault connection created successfully."
     $completed = $true
   }
@@ -132,7 +197,7 @@ while (-not $completed) {
   }
 }
 
-Write-Host "Registering Azure Data Lake data source..."
+#------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------
 # CONTROL PLANE OPERATOR: DISABLE PUBLIC NETWORK ACCESS
